b18bfa1a9f188eda47e5f038378ed84200971dfa
gilnamkim <dustin0627@naver.com>
2023-08-07 19:06:38 +0900

    scanf로 금액 입력받기

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello world!\n");
+	float won = 0;
+
+	printf("환전을 원하는 금액을 입력하세요(원화) : ");
+	scanf("%f",&won);
+
+	printf("원화는 금액은 %.f\n",won);
 
 	return 0;
 }
