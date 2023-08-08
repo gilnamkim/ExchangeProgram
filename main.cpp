1ef9adaac2f6c91f3bfb3186ef3da2e92684913b
gilnamkim <dustin0627@naver.com>
2023-08-08 15:54:33 +0900

    입출력test

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
+const float USD = 1316.07, JPY = 9.20, EUR = 1448.81, CNY = 182.49, GBP = 1680.07;
+
 int main() {
-	
-	cout << "hello world!" << endl;
-	
+
+	float inputWon;
+
+	cout << "환전을 원하는 금액을 입력하세요(KRW) : ";
+
+	cin >> inputWon;
+
+	cout << "환전을 원하는 금액은...." << inputWon << endl;
+
 	return 0;
 }
